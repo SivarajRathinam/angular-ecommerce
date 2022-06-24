4721e0f20b56163360aed45d276eac8e9cd3f68e
SivarajRathinam <sivaraj@zibo.co>
2022-06-24 19:59:27 +0530

    adding slider to homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import {MatGridListModule,} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { SliderComponent } from './home/slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatIconModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/slider/slider.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations';
+
+const animations = trigger('img-animation', [
+  transition(':enter', [
+    style({ opacity: 0 }),
+    animate('300ms', style({ opacity: 1 }))
+  ]),
+  transition(':leave', [
+    animate('300ms', style({ opacity: 0 }))
+])
+]);
+@Component({
+  selector: 'app-slider',
+  templateUrl: './slider.component.html',
+  styleUrls: ['./slider.component.scss'],
+  animations: [animations]
+})
+export class SliderComponent implements OnInit,OnDestroy {
+  @Input() images: string[];
+  currentImageIndex: number;
+  sliderInterval: any;
+  imageUrl: string;
+  constructor() {
+    this.images = [];
+    this.imageUrl = '';
+    this.currentImageIndex = 0;
+  }
+  updateImage(){
+    console.log('update image')
+    if (this.images.length > this.currentImageIndex+1) {
+      this.currentImageIndex += 1;
+      return;
+    }
+    this.currentImageIndex = 0;
+  }
+  startIntervalTimer() :void {
+    this.sliderInterval = setInterval(this.updateImage.bind(this), 5000);
+  } 
+  ngOnInit(): void {
+    if (this.images.length > 0) {
+      this.imageUrl = this.images[0];
+      this.startIntervalTimer()
+    }
+  }
+  selectIndex(index:number){
+    this.currentImageIndex = index
+  }
+  ngOnDestroy(): void {
+    clearInterval(this.sliderInterval)
+  }
+}
